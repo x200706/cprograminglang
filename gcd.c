caf45d16b502bf73f830d1933cfce3049e7ed1cb
chi200706 <chi200706@gmail.com>
2025-03-21 06:38:16 +0000

    最大公因數初版

---
+++ gcd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int gcd(int x, int y);
+
+int main(void){
+    int a;
+    int b;
+    printf("Enter two integer: ");
+    scanf("%d%d",&a, &b);
+    int ans = gcd(a, b);
+    printf("Greatest common divisor of %d and %d is %d", a, b, ans);
+}
+
+int gcd(int x, int y) {
+    int ans = 0;
+    if (y == 0){
+        return x;
+    }
+    else{
+        return gcd(y, x % y);
+    }
+}
