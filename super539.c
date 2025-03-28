57a2244c45f50c9cd89a36b964475b21d006f271
Crystal <99391710+x200706@users.noreply.github.com>
2025-03-28 15:47:47 +0800

    台彩539的改版草稿

---
+++ super539.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+   int arr[5] = {0};
+   for (int i = 1; i <= 5; ++i) {
+      num = "%10d", 1 + (rand() % 39;
+      printf());
+      if (i % 5 == 0) {
+         puts("");
+      } 
+   } 
+} 
+
+// 二分搜尋那題用fig 6.19改成遞迴就ok了
